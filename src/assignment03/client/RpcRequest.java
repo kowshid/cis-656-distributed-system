2e43b8cd5edaf8ebad3ade9154de4b27ab54d7d5
Ahsanul Kabir <kowshid@gmail.com>
2025-09-30 22:18:51 -0400

    Assignment02: Implement Client Side Files

---
+++ src/assignment03/client/RpcRequest.java
@@ -0,0 +1,20 @@
+package assignment03.client;
+
+import java.io.Serializable;
+
+public class RpcRequest implements Serializable {
+
+
+    public static final String EMPTY_STRING = "";
+    private final long serialVersionUID = 1L;
+
+    private String message;
+
+    public RpcRequest(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
